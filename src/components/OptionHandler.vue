ff1dccca30522c08eccf75035c00e546f66d17ac
김성경 <ella@gimseong-gyeongs-MacBook-Air.local>
2023-01-02 23:55:01 +0900

    modify favicon, add logo

--- src/components/OptionHandler.vue
+++ src/components/OptionHandler.vue
@@ -7,6 +7,7 @@ defineProps({
 
 <template>
   <div class="option-container">
+    <h1 class="header"></h1>
     <label for="sandwichMax">샌드위치</label>
     <input id="sandwichMax" type="text" value="1">
     <label for="breadMax">빵</label>
@@ -23,5 +24,13 @@ defineProps({
     .hello {
       color: yellow;
     }
+  }
+  .header{
+    background-image: url('../assets/subway_logo.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    width: 300px;
+    height: 100px;
+    margin: 10px auto;
   }
 </style>
